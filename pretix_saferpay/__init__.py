ec72a8cc0d7847b85275691ba77c46469e3d867e
Raphael Michel <mail@raphaelmichel.de>
2019-05-09 16:04:14 +0200

    Bump version

--- pretix_saferpay/__init__.py
+++ pretix_saferpay/__init__.py
@@ -11,7 +11,7 @@ class PluginApp(AppConfig):
         author = 'Raphael Michel'
         description = ugettext_lazy('This allows to accept payments through saferpay.')
         visible = True
-        version = '1.0.0'
+        version = '1.1.0'
 
     def ready(self):
         from . import signals, tasks  # NOQA

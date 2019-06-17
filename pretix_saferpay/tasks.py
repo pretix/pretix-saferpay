877c87d84fe4661c382733f848f9d8f7bf971642
Raphael Michel <mail@raphaelmichel.de>
2019-06-17 10:29:37 +0200

    Disable scopes for capture task

--- pretix_saferpay/tasks.py
+++ pretix_saferpay/tasks.py
@@ -1,8 +1,11 @@
+from django_scopes import scopes_disabled
+
 from pretix.base.models import OrderPayment
 from pretix.celery_app import app
 
 
 @app.task()
+@scopes_disabled()
 def capture_task(payment_id, **kwargs):
     from .views import capture
 

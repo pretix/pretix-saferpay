6f128328bfd43c90632664b79f792edc1b439489
Mira <weller@rami.io>
2024-04-04 19:48:05 +0200

    Remove broken method payment_can_retry (#10)

--- pretix_saferpay/payment.py
+++ pretix_saferpay/payment.py
@@ -250,9 +250,6 @@ class SaferpayMethod(BasePaymentProvider):
         }
         return template.render(ctx)
 
-    def payment_can_retry(self, payment):
-        return self._is_still_available(order=payment.order)
-
     def payment_pending_render(self, request, payment) -> str:
         if payment.info:
             payment_info = json.loads(payment.info)

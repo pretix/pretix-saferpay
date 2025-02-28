3caef33405e8e0e3ac8c020cae329f8468e33d3c
Martin Gross <gross@rami.io>
2025-02-28 10:22:42 +0100

    Clarify the merchant users of this platform; Uppercase PAYONE

--- pretix_saferpay/apps.py
+++ pretix_saferpay/apps.py
@@ -12,13 +12,24 @@ class PluginApp(AppConfig):
         name = gettext_lazy("Saferpay (SIX)")
         author = "Raphael Michel"
         category = "PAYMENT"
-        description = gettext_lazy(
-            "Accept payments through Saferpay, a payment method offered by Worldline (formerly SIX Payment Services)."
-        )
         visible = True
         picture = "pretix_saferpay/logo.svg"
         version = __version__
         compatibility = "pretix>=4.20.0"
+
+        @property
+        def description(self):
+            t = gettext_lazy(
+                "Accept payments through Saferpay, a payment method offered by Worldline (formerly SIX Payment Services)."
+            )
+            t += '<div class="text text-info"><span class="fa fa-info-circle"></span> '
+            t += gettext_lazy(
+                "Sometimes also referred to as PAYONE. Use this extension, if PAYONE has provided you with a "
+                "<em>Customer ID</em>."
+            )
+            t += '</div>'
+
+            return t
 
     def ready(self):
         from . import signals, tasks  # NOQA
